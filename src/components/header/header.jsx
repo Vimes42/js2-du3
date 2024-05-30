7bb017bc9557f348ee3b9d45e4a7f671e9aac7f6
Lenka Polcerova <lenka.polcerova@gmail.com>
2024-05-30 23:51:22 +0200

    kroky 1-7

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import './header.css';
+
+export const Header = () => {
+    return (
+        <>
+        <header>Realitní kancelář</header>
+        <nav>
+            <a href="http://localhost:5173/dum01.html">Dům 01</a>
+            <a href="http://localhost:5173/dum02.html">Dům 02</a>
+            <a href="http://localhost:5173/dum03.html">Dům 03</a>
+            <a href="http://localhost:5173/dum04.html">Dům 04</a>
+        </nav>
+        </>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,18 +1,10 @@
 import { render } from '@czechitas/render';
 import '../global.css';
 import './index.css';
+import { Header } from '../components/header/header'
 
 document.querySelector('#root').innerHTML = render(
   <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v JavaScriptu s JSX. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-kodim-app">create-kodim-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <Header />
   </div>
 );
